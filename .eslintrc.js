850292f7040c3db0dce687cd1189011b6850cb12
seoyoung park <ongddree@gmail.com>
2022-01-25 14:01:49 +0900

    fix: eslint 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,9 @@ module.exports = {
   },
   extends: ["eslint:recommended", "airbnb", "plugin:prettier/recommended"],
   rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
     "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
   },
+
   // 위처럼 플러그인 이름 + / + rule 이름을 키로 하고 뒤는 공식문서에 나온 설명대로 원하는 규칙으로 설정해줍니다.
 };
