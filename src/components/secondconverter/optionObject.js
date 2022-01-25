955cd44d9bce38ba0683a1ce91c72d19000299bc
seoyoung park <ongddree@gmail.com>
2022-01-26 02:00:34 +0900

    Refactor : 통화 리스트 추상화

---
+++ src/components/secondconverter/optionObject.js
@@ -0,0 +1,8 @@
+export const optionObject = [
+  { id: 0, value: 'USD', title: 'USD', content: 'USD 환율' },
+  { id: 1, value: 'CAD', title: 'CAD', content: 'CAD 환율' },
+  { id: 2, value: 'KRW', title: 'KRW', content: 'KRW 환율' },
+  { id: 3, value: 'HKD', title: 'HKD', content: 'HKD 환율' },
+  { id: 4, value: 'JPY', title: 'JPY', content: 'JPY 환율' },
+  { id: 5, value: 'CHY', title: 'CHY', content: 'CHY 환율' },
+];
